a619a16a34f9f3c819b2177fbad7162d266293a3
azaben85 <azaben85@gmail.com>
2022-11-07 19:03:35 +0200

    init

---
+++ android/app/src/main/kotlin/com/example/todo_db_provide/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todo_db_provide
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/screens/todo_main_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodoMainScreen extends StatelessWidget {
+  const TodoMainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ToDo Main Screen'),
+      ),
+      body: Container(),
+    );
+  }
+}
